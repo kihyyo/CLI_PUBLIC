103972f174250a15a17cefb8a38fdb022337a776
kihyyo <83176214+kihyyo@users.noreply.github.com>
2024-09-17 15:40:44 +0900

    Update site_kakao_page.py

--- reading/site_kakao_page.py
+++ reading/site_kakao_page.py
@@ -1,13 +1,5 @@
 from email import header
 import os, sys, traceback, json, urllib.parse, requests, argparse, yaml, platform, time, threading, re, base64, fnmatch
-import traceback, unicodedata
-from datetime import datetime
-import urllib.request as py_urllib2
-import urllib.parse as py_urllib #urlencode
-from bs4 import BeautifulSoup
-
-from lxml import html, etree
-import xmltodict
 
 if platform.system() == 'Windows':
     sys.path += ["C:\SJVA3\lib2", "C:\SJVA3\data\custom", "C:\SJVA3_DEV"]
@@ -28,7 +20,7 @@ class SiteKakaoPage():
 
     headers = {
     'Content-Type': 'application/json',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.79',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36',
     }
     @classmethod
     def search(cls, title, auth=''):
@@ -87,7 +79,7 @@ class SiteKakaoPage():
                     entity = {}
                     entity['code'] = re.search('\d+',data['id']).group()
                     entity['title'] = re.search('작품,([^,]+)',data['altText']).group().replace('작품,','').strip()
-                    entity['premiered'] = re.search('\d{2}\.\d{2}\.\d{2}', data['altText']).group().replace('.','').strip()
+                    entity['premiered'] = re.search('\d{4}\.\d{2}\.\d{2}', data['altText']).group().replace('.','').strip()
                     entity['author'] = re.search('작가\s([^,]+)',data['altText']).group().replace('작가 ','').strip()
                     entity['overall'] = data['altText']
                     ret.append(entity)
@@ -99,22 +91,27 @@ class SiteKakaoPage():
             logger.error(traceback.format_exc())
 
     @classmethod
-    def info(cls, code):
+    def info(cls, code, select_item):
+        encoded_string = urllib.parse.quote(select_item['title'])
+        cls.headers['Referer'] = f"https://page.kakao.com/search/result?keyword={encoded_string}",
         try:
-            url = f"https://page.kakao.com/_next/data/2.12.1/ko/content/{code}.json?tab_type=about&seriesId={code}"
-            res = requests.get(url, headers=cls.headers).json()
+            url = f"https://page.kakao.com/graphql"
+            data = {
+            "query": "\n    query contentHomeInfo($seriesId: Long!) {\n  contentHomeInfo(seriesId: $seriesId) {\n    about {\n      id\n      themeKeywordList {\n        uid\n        title\n        scheme\n      }\n      description\n      screenshotList\n      authorList {\n        id\n        name\n        role\n        roleDisplayName\n      }\n      detail {\n        id\n        publisherName\n        retailPrice\n        ageGrade\n        category\n        rank\n      }\n      guideTitle\n      characterList {\n        thumbnail\n        name\n        description\n      }\n      detailInfoList {\n        title\n        info\n      }\n    }\n    recommend {\n      id\n      seriesId\n      list {\n        ...ContentRecommendGroup\n      }\n    }\n  }\n}\n    \n    fragment ContentRecommendGroup on ContentRecommendGroup {\n  id\n  impLabel\n  type\n  title\n  description\n  items {\n    id\n    type\n    ...PosterViewItem\n  }\n}\n    \n\n    fragment PosterViewItem on PosterViewItem {\n  id\n  type\n  showPlayerIcon\n  scheme\n  title\n  altText\n  thumbnail\n  badgeList\n  ageGradeBadge\n  statusBadge\n  subtitleList\n  rank\n  rankVariation\n  ageGrade\n  selfCensorship\n  eventLog {\n    ...EventLogFragment\n  }\n  seriesId\n}\n    \n\n    fragment EventLogFragment on EventLog {\n  fromGraphql\n  click {\n    layer1\n    layer2\n    setnum\n    ordnum\n    copy\n    imp_id\n    imp_provider\n  }\n  eventMeta {\n    id\n    name\n    subcategory\n    category\n    series\n    provider\n    series_id\n    type\n  }\n  viewimp_contents {\n    type\n    name\n    id\n    imp_area_ordnum\n    imp_id\n    imp_provider\n    imp_type\n    layer1\n    layer2\n  }\n  customProps {\n    landing_path\n    view_type\n    helix_id\n    helix_yn\n    helix_seed\n    content_cnt\n    event_series_id\n    event_ticket_type\n    play_url\n    banner_uid\n  }\n}\n    ",
+            "variables": {
+                "seriesId": code
+            }
+            }
+            res = requests.post(url, json=data, headers=cls.headers)
             ret = {}
-            ret['title'] = res['pageProps']['metaInfo']['ogTitle']
-            ret['desc'] = res['pageProps']['metaInfo']['description']
-            ret['poster'] = 'https:' + res['pageProps']['metaInfo']['image']
-            ret['author'] = res['pageProps']['metaInfo']['author']
-            ret['publisher'] = res['pageProps']['dehydratedState']['queries'][0]['state']['data']['contentHomeAbout']['detail']['publisherName']
-            url = f'https://page.kakao.com/content/{code}'
-            response = requests.get(url, headers=cls.headers)
-            soup = BeautifulSoup(response.text, 'html.parser')
-            ret['is_completed'] = soup.find("meta", attrs={"property": "article:section"})['content'].replace('연재중', '연재')
+            ret['title'] = select_item['title']
+            ret['desc'] = res['data']['contentHomeInfo']['about']['description']
+            ret['poster'] = 'https:' + res['data']['contentHomeInfo']['about']['screenshotList'][0]
+            ret['author'] = res['data']['contentHomeInfo']['about']['authorList'][0]['name']
+            ret['publisher'] = res['data']['contentHomeInfo']['about']['detail']['publisherName']
+            ret['is_completed'] = '연재' if '연재중' in select_item['overall'] else '완결'
             ret['tag'] = ['카카오페이지']
-            ret['genre'] = res['pageProps']['dehydratedState']['queries'][0]['state']['data']['contentHomeAbout']['detail']['category'].split(' | ')
+            ret['genre'] = res['data']['contentHomeInfo']['about']['detail']['category'].split(' | ')
             return ret
         except Exception as exception:
             logger.error('Exception:%s', exception)
